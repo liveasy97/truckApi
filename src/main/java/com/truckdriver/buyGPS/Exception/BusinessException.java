ed1447a9fe4384e2c55a5c04ce46de68e6eed76c
{{SAMRAT97}} <{{kharanshugupta@gmail.com}}>
2021-09-25 16:51:33 +0530

    buyGpsAPI merge into truckdriver

---
+++ src/main/java/com/truckdriver/buyGPS/Exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.truckdriver.buyGPS.Exception;
+
+public class BusinessException extends RuntimeException
+{
+
+	public BusinessException(String s) 
+	{
+		super(BusinessException.generateMessage(s));
+	}
+
+	private static String generateMessage(String s) 
+	{
+		return "Error" + s;
+	}
+}
---
+++ src/main/java/com/truckdriver/buyGPS/Response/DeleteBuyGPSResponse.java
@@ -0,0 +1,9 @@
+package com.truckdriver.buyGPS.Response;
+
+import lombok.Data;
+
+@Data
+public class DeleteBuyGPSResponse 
+{
+   private String message;
+}
